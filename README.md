3817def469d8023b06641038bd9d2092105902f0
Miguel Latre <latre@unizar.es>
2019-12-05 16:42:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ficheros binarios
+
+Código de las transparencias del tema 15 (ficheros binarios) de la asignatura Programación 1. Escuela de Ingeniería y Arquitectura de la Universidad de Zaragoza.
+
+El código está estructurado en un área de trabajo de CodeLite (fichero `tema-15-ficheros-binarios.workspace`) con varios proyectos CodeLite.
+
+El proyecto `creacion-fichero-primos` necesita del módulo «calculos» del proyecto `calculadora` del [repositorio del tema 7 (desarrollo modular)](https://github.com/prog1-eina/tema-07-desarrollo-modular). El proyecto ha sido configurado suponiendo que el directorio del área de trabajo del código del tema 7 se encuentra en un directorio denominado `tema-07-desarrollo-modular` ubicado en la misma carpeta que el directorio de esta área de trabajo. Los ficheros que forman el proyecto y la lista de inclusión pueden modificarse desde CodeLite si fuese preciso. También podría cambiarse la configuración para que utilizara el mismo módulo desde el área de trabajo `practica3`.
---
+++ fichero-notas/fichero-notas-main.cpp
@@ -0,0 +1,99 @@
+﻿/********************************************************************************\
+ * Curso de Programación 1. Tema 15 (Ficheros binarios)
+ * Autores: Miguel Ángel Latre
+ * Última revisión: 5 de diciembre de 2019
+ * Resumen: Funciones que trabajan con un fichero binario de pares NIP-nota.
+ * Codificación de caracteres original de este fichero: UTF-8 con BOM
+\********************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+const char NOMBRE_FICHERO[] = "prog1.dat";
+
+
+/*
+ * Pre:  ---
+ * Post: Ha creado un fichero binario de nombre «nombreFichero» compuesto por
+ *       una secuencia de pares (NIP, nota) solicitados interactivamente al
+ *       usuario.
+ */
+void crearFicheroNotas(const char nombreFichero[]) {
+    ofstream f;
+    f.open(nombreFichero, ios::binary);
+    if (f.is_open()) {
+        cout << "Introduzca un NIP (0 para acabar): ";
+        int nip;
+        cin >> nip;
+        while (nip != 0) {
+            cout << "Introduzca una nota: ";
+            double nota;
+            cin >> nota;
+
+            f.write(reinterpret_cast<const char*>(&nip), sizeof(nip));
+            f.write(reinterpret_cast<const char*>(&nota), sizeof(nota));
+
+            cout << "Introduzca un NIP (0 para acabar): ";
+            cin >> nip;
+        }
+        f.close();
+    }
+    else {
+        cerr << "No se ha podido escribir en el fichero \"" << nombreFichero
+             << "\"" << endl;
+    }
+}
+
+
+/*
+ * Pre:  «nombreFichero» es el nombre de un fichero existente binario cuya
+ *       estructura consiste en una secuencia de pares (NIP, nota), de tipos
+ *       int y double, respectivamente.
+ * Post: Ha mostrado en la pantalla del contenido del fichero de nombre
+ *       «nombreFichero», de acuerdo con el siguiente formato de ejemplo:
+ *         NIP   Nota
+ *       ------------
+ *       487524   7.9
+ *       454844  10.0
+ *       567896   6.3
+ */
+void mostrarFicheroNotas(const char nombreFichero[]) {
+    ifstream f;
+    f.open(nombreFichero, ios::binary);
+    if (f.is_open()) {
+        cout << "  NIP   Nota" << endl;
+        cout << "------------" << endl;
+        cout << fixed << setprecision(1);
+        int nip;
+        f.read(reinterpret_cast<char*>(&nip), sizeof(nip));
+        while(!f.eof()) {
+            double nota;
+            f.read(reinterpret_cast<char*>(&nota), sizeof(nota));
+            cout << setw(6) << nip << " " << setw(5) << nota << endl;
+            f.read(reinterpret_cast<char*>(&nip), sizeof(nip));
+        }
+        f.close();
+    } else {
+        cerr << "No se ha podido leer el fichero \"" << nombreFichero
+             << "\"" << endl;
+    }
+}
+
+
+/*
+ * Programa que crea un fichero binario de nombre «NOMBRE_FICHERO» compuesto por
+ * una secuencia de pares (NIP, nota) solicitados interactivamente al usuario y,
+ * tras ello, muestra en la pantalla del contenido del fichero recién creado, de
+ * acuerdo con el siguiente formato de ejemplo:
+ *         NIP   Nota
+ *       ------------
+ *       487524   7.9
+ *       454844  10.0
+ *       567896   6.3
+ */
+int main() {
+    crearFicheroNotas(NOMBRE_FICHERO);
+    mostrarFicheroNotas(NOMBRE_FICHERO);
+    return 0;
+}
