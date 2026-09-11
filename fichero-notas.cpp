c9bd9d15660df73fb1dd1ea1d5f6dff1a86c4b63
Miguel Ángel Latre Abadía <latre@unizar.es>
2026-09-11 17:35:14 +0200

    Remove modified date from all file headers

--- fichero-notas.cpp
+++ fichero-notas.cpp
@@ -1,7 +1,6 @@
 ﻿/******************************************************************************
  * Curso de Programación 1. Tema 15 (Ficheros binarios)
  * Autores: Miguel Ángel Latre
- * Última revisión: 2 de diciembre de 2021
  * Resumen: Funciones que trabajan con un fichero binario de pares NIP-nota.
  * Nota: El programa completo está contenido en este fichero, por lo que puede 
  *       compilarse y ejecutarse con la extensión Code Runner de Visual Studio 
